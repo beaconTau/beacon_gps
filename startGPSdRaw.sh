7bc44c09476096c01a9378e2a7b0e5d16af68977
Cosmin Deaconu <cozzyd@gmail.com>
2022-11-05 09:56:09 -0500

    try to set baud rate on newer devices

--- startGPSdRaw.sh
+++ startGPSdRaw.sh
@@ -2,7 +2,21 @@
 
 sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
-#sudo killall gpsd
+sudo killall gpsd
+
+
+
+#if we have REVE, set speed to 57600 
+if [[ -f /REV ]] ; 
+then 
+  rev=`cat /REV` 
+  echo "rev is E" 
+  if [[ $rev == "E" ]] 
+  then 
+    echo "Changing baud rate" 
+    ubxtool -s 9600 -S 57600 /dev/ttyGPS
+  fi
+fi
 
 stty -F /dev/ttyO4 57600
 sudo gpsd /dev/ttyO4 -F /var/run/gpsd.sock 
