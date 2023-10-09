d8cd134a79c8c26731b163483a78b2b5d024550b
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-10-09 06:32:18 +0000

    device as argument

--- collect.sh
+++ collect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-SECS=${1-3600}
+WHICH=${1-0}
+SECS=${2-3600}
 
-ofile=/data/gps/`date -Is`.ubx 
-gpspipe -R "-x $SECS" > $ofile && gzip $ofile 
+ofile=/data/gps/if0${WHICH}-`date -Is`.ubx 
+gpspipe -R "-x $SECS" localhost:2947:/dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_016BD3C4-if0${WHICH}-port0 > $ofile && gzip $ofile 
 
