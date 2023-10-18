d7105cfc5dfb08210ecb9729f9261399d40f5754
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-10-18 16:53:58 +0000

    make baud rate setting make more sense

--- startGPSdRaw.sh
+++ startGPSdRaw.sh
@@ -10,10 +10,14 @@ sudo killall gpsd
 gps0=/dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_016BD3C4-if00-port0
 gps1=/dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_016BD3C4-if01-port0
 echo "Changing baud rate" 
-ubxtool -s 38400 -S 57600 -f $gps0
-ubxtool -s 38400 -S 57600 -f $gps1
-stty -F $gps0 57600 
-stty -F $gps1 57600 
+# this starts as 38.4
+stty -F $gps0 38400 
+stty -F $gps1 38400 
+#change it to 115.2
+ubxtool -s 38400 -S 115200 -f $gps0
+ubxtool -s 38400 -S 115200 -f $gps1
+stty -F $gps0 115200 
+stty -F $gps1 115200 
 
 sudo gpsd $gps0 $gps1  -F /var/run/gpsd.sock 
 sudo mkdir -p /data/gps  && sudo chown beacon:beacon /data/gps
